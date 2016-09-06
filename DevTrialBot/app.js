def6b7d51173348c95c59c0fc815f9302c04ccfc
Solomon Watkins <silverwatkins@gmail.com>
2016-09-06 13:46:22 -0400

    Initial commit.

---
+++ DevTrialBot/app.js
@@ -0,0 +1,12 @@
+app.post('/hello', function (req, res, next) {
+  var userName = req.body.user_name;
+  var botPayload = {
+    text : 'Hello ' + userName + ', welcome to CodestartDeepstack Slack channel! I\'ll be your public personal assistant.'
+  };
+  // Loop otherwise..
+  if (userName !== 'slackbot') {
+    return res.status(200).json(botPayload);
+  } else {
+    return res.status(200).end();
+  }
+});
